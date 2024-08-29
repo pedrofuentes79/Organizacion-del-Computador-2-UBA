ab04c426b89ecc72991445e81945e2e6e5558f10
Pedro <pedrofuentes7799@gmail.com>
2024-08-29 15:30:18 -0300

    intenta mejorar classify chars, no pude salir del segmentation fault. en donde estamos accediendo a un puntero que no debemos? o es otra cosa?

--- TP1-a/src/classify_chars.c
+++ TP1-a/src/classify_chars.c
@@ -5,30 +5,35 @@
 #include <ctype.h>
 
 void classify_chars_in_string(char* string, char** vowels_and_cons) {
-    vowels_and_cons = malloc(2 * 64 * sizeof(char));
+    vowels_and_cons = malloc(2 * sizeof(char*));
+
+    vowels_and_cons[0] = malloc(64 * sizeof(char));
+    vowels_and_cons[1] = malloc(64 * sizeof(char)); 
+
+
+    // inicializo como strings vacios
+    vowels_and_cons[0][0] = '\0';
+    vowels_and_cons[1][0] = '\0';
+
+
     int i = 0;
     while(string[i] != '\0'){
         char value = toupper(string[i]);
         if(value == 'A' || value == 'E'|| value == 'I' || value == 'O' || value == 'U'){
             size_t len = strlen(vowels_and_cons[0]);
-            vowels_and_cons[0] = realloc(vowels_and_cons[0],(len+1)*sizeof(char));
+            // reallocamos el array para agregar un nuevo caracter
+            vowels_and_cons[0] = realloc(vowels_and_cons[0],(len+2)*sizeof(char));
             vowels_and_cons[0][len] = string[i];
+            vowels_and_cons[0][len+1] = '\0';
         }
-        else if ('A' <= value <= 'Z'){
+        else if ('A' <= value && value <= 'Z'){
             size_t len = strlen(vowels_and_cons[1]);
-            vowels_and_cons[1] = realloc(vowels_and_cons[1],(len+1)*sizeof(char));
+            vowels_and_cons[1] = realloc(vowels_and_cons[1],(len+2)*sizeof(char));
             vowels_and_cons[1][len] = string[i];
+            vowels_and_cons[1][len+1] = '\0';
         }
         i++;
     }
-    // Finalizamos los arrays agregando los '\0'   
-    size_t len1 = strlen(vowels_and_cons[0]);
-    vowels_and_cons[0] = realloc(vowels_and_cons[0],(len1+1)*sizeof(char));
-    vowels_and_cons[0][len1] = '\0';
-
-    size_t len2 = strlen(vowels_and_cons[1]);
-    vowels_and_cons[1] = realloc(vowels_and_cons[1],(len1+1)*sizeof(char));
-    vowels_and_cons[1][len1] = '\0';
 }
 
 void classify_chars(classifier_t* array, uint64_t size_of_array) {
