f91e21f364774ff097a9fc118b3fd9aee6ea5ce5
Pedro <pedrofuentes7799@gmail.com>
2024-08-29 15:34:33 -0300

    segmentation fault arreglado!! habia que alocar la memoria antes de llamar a la funcion classify_chars_in_strings

--- TP1-a/src/classify_chars.c
+++ TP1-a/src/classify_chars.c
@@ -5,15 +5,15 @@
 #include <ctype.h>
 
 void classify_chars_in_string(char* string, char** vowels_and_cons) {
-    vowels_and_cons = malloc(2 * sizeof(char*));
+    // vowels_and_cons = malloc(2 * sizeof(char*));
 
-    vowels_and_cons[0] = malloc(64 * sizeof(char));
-    vowels_and_cons[1] = malloc(64 * sizeof(char)); 
+    // vowels_and_cons[0] = malloc(64 * sizeof(char));
+    // vowels_and_cons[1] = malloc(64 * sizeof(char)); 
 
 
-    // inicializo como strings vacios
-    vowels_and_cons[0][0] = '\0';
-    vowels_and_cons[1][0] = '\0';
+    // // inicializo como strings vacios
+    // vowels_and_cons[0][0] = '\0';
+    // vowels_and_cons[1][0] = '\0';
 
 
     int i = 0;
@@ -38,6 +38,15 @@ void classify_chars_in_string(char* string, char** vowels_and_cons) {
 
 void classify_chars(classifier_t* array, uint64_t size_of_array) {
     for(uint64_t i = 0; i < size_of_array; i++){
+        // alocar memoria ACA
+        array[i].vowels_and_consonants = malloc(2 * sizeof(char*));
+        array[i].vowels_and_consonants[0] = malloc(64 * sizeof(char));
+        array[i].vowels_and_consonants[1] = malloc(64 * sizeof(char));
+
+        // inicializar strings
+        array[i].vowels_and_consonants[0][0] = '\0';
+        array[i].vowels_and_consonants[1][0] = '\0';
+
         classify_chars_in_string(array[i].string, array[i].vowels_and_consonants);
     }
 }
