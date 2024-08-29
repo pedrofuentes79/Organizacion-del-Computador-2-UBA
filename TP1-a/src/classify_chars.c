70e1eacaebb7237131031b60de41393709a98fdb
Pedro <pedrofuentes7799@gmail.com>
2024-08-29 19:10:50 -0300

    classify chars pasa todos los tests sin errores de memoria! al parecer los caracteres especiales contaban como consonantes...

--- TP1-a/src/classify_chars.c
+++ TP1-a/src/classify_chars.c
@@ -5,17 +5,6 @@
 #include <ctype.h>
 
 void classify_chars_in_string(char* string, char** vowels_and_cons) {
-    // vowels_and_cons = malloc(2 * sizeof(char*));
-
-    // vowels_and_cons[0] = malloc(64 * sizeof(char));
-    // vowels_and_cons[1] = malloc(64 * sizeof(char)); 
-
-
-    // // inicializo como strings vacios
-    // vowels_and_cons[0][0] = '\0';
-    // vowels_and_cons[1][0] = '\0';
-
-
     int i = 0;
     while(string[i] != '\0'){
         char value = toupper(string[i]);
@@ -26,7 +15,7 @@ void classify_chars_in_string(char* string, char** vowels_and_cons) {
             vowels_and_cons[0][len] = string[i];
             vowels_and_cons[0][len+1] = '\0';
         }
-        else if ('A' <= value && value <= 'Z'){
+        else {
             size_t len = strlen(vowels_and_cons[1]);
             vowels_and_cons[1] = realloc(vowels_and_cons[1],(len+2)*sizeof(char));
             vowels_and_cons[1][len] = string[i];
