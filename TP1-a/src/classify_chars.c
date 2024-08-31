9529c41eb46621099a2795ac7038409a97088b5d
Pedro <pedrofuentes7799@gmail.com>
2024-08-31 16:56:35 -0300

    agrega primer implementacion adicional de classify chars; usa punteros a vowels y consonants en vez de un arreglo de dos posiciones; falta armar una implementacion distinta, probablemente lo mas facil sea armar un arreglo de structs que contengan todo. Es decir, a la funcion classify_chars_v3 le querriamos pasar un puntero a ese arreglo de structs, y cada struct va a tener un string, sus vocales y sus consonantes.

--- TP1-a/src/classify_chars.c
+++ TP1-a/src/classify_chars.c
@@ -29,6 +29,7 @@ void classify_chars(classifier_t* array, uint64_t size_of_array) {
     for(uint64_t i = 0; i < size_of_array; i++){
         // alocar memoria ACA
         array[i].vowels_and_consonants = malloc(2 * sizeof(char*));
+        // podriamos usar calloc, pero no es necesario ya que solo queremos setear el primer valor en \0
         array[i].vowels_and_consonants[0] = malloc(64 * sizeof(char));
         array[i].vowels_and_consonants[1] = malloc(64 * sizeof(char));
 
@@ -38,4 +39,56 @@ void classify_chars(classifier_t* array, uint64_t size_of_array) {
 
         classify_chars_in_string(array[i].string, array[i].vowels_and_consonants);
     }
+}
+
+
+void classify_chars_in_string_v2(char* string, char* vowels, char* cons) {
+    int i = 0;
+    while(string[i] != '\0'){
+        char value = toupper(string[i]);
+        if(value == 'A' || value == 'E'|| value == 'I' || value == 'O' || value == 'U'){
+            size_t len = strlen(vowels);
+            // reallocamos el array para agregar un nuevo caracter
+            vowels = realloc(vowels,(len+2)*sizeof(char));
+            vowels[len] = string[i];
+            vowels[len+1] = '\0';
+        }
+        else {
+            size_t len = strlen(cons);
+            cons = realloc(cons,(len+2)*sizeof(char));
+            cons[len] = string[i];
+            cons[len+1] = '\0';
+        }
+        i++;
+    }
+}
+
+// esta funcion recibe un array de strings y dos punteros, uno al arreglo de vocales y otro al arreglo de consonantes
+void classify_chars_v2(char** strings, uint64_t size_of_array, char**vowels, char** cons) {
+    for(uint64_t i = 0; i < size_of_array; i++){
+        // allocate memory for vowels and cons
+        vowels[i] = malloc(64 * sizeof(char));
+        cons[i] = malloc(64 * sizeof(char));
+        vowels[i][0] = '\0';
+        cons[i][0] = '\0';
+    
+        classify_chars_in_string_v2(strings[i], vowels[i], cons[i]);
+    }
+}
+
+
+
+
+
+int main() {
+    // test the v2 functions
+    char* strings[] = {"orga", "dos", "segundo", "cuatri"};
+    char* vowels[4];
+    char* cons[4];
+    classify_chars_v2(strings, 4, vowels, cons);
+    for(int i = 0; i < 4; i++){
+        printf("Vowels: %s\n", vowels[i]);
+        printf("Cons: %s\n", cons[i]);
+    }
+    return 0;
 }
