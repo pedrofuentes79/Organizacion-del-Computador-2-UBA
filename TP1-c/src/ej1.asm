8296bbded94798e71e607b3d02906951adcacfc8
Pedro <pedrofuentes7799@gmail.com>
2024-09-15 17:59:21 -0300

    pasa los tests

--- TP1-c/src/ej1.asm
+++ TP1-c/src/ej1.asm
@@ -1,16 +1,20 @@
-section .rodata ; Poner acá todas las máscaras y coeficientes que necesiten para el filtro
-	; mascaras para aplicar el filtro
-	mask_blue:  times 4 dd 0x00_FF_00_00
-	mask_green: times 4 dd 0x00_00_FF_00
-	mask_red: times 4 dd 0x00_00_00_FF
-	alpha: times 4 dd 0x00_00_00_FF
-	
-	; coeficientes para el filtro (calculo luminosidad)
-	coef_red times 4 dd 0.2126
-	coef_green times 4 dd 0.7152
-	coef_blue times 4 dd 0.0722
-	alpha: times 4 dd 0x00_00_00_FF
+section .rodata
+; Poner acá todas las máscaras y coeficientes que necesiten para el filtro
+; coeficientes escala de grises
 
+	; mascaras para aplicar el filtro
+	mask_coef_blue:  times 4 dd 0x00_FF_00_00
+	mask_coef_green: times 4 dd 0x00_00_FF_00
+	mask_coef_red:   times 4 dd 0x00_00_00_FF
+
+	; coeficientes para el filtro (calculo luminosidad)
+	coef_red: times 4 dd 0.2126
+	coef_green: times 4 dd 0.7152
+	coef_blue: times 4 dd 0.0722
+	coef_alph: times 4 dd 0xFF_00_00_00
+
+mask_shuf: db 0x00, 0x00, 0x00, 0x00, 0x01, 0x01, 0x01, 0x01, 0x02, 0x02, 0x02, 0x02, 0x03, 0x03, 0x03, 0x03
+;mask_xor: times 4 db 0xFF, 0x00, 0x00, 0x00
 
 section .text
 
@@ -44,78 +48,107 @@ EJERCICIO_1_HECHO: db TRUE ; Cambiar por `TRUE` para correr los tests.
 ; ```
 ;
 ; Parámetros:
-;   - dst [rdi]:    La imagen destino. Está a color (RGBA) en 8 bits sin signo por canal. 
-;   - src [rsi]:    La imagen origen A. Está a color (RGBA) en 8 bits sin signo por canal.
-;   - width [rdx]:  El ancho en píxeles de `src` y `dst`.
-;   - height [rcx]: El alto en píxeles de `src` y `dst`.
+;   - dst:    La imagen destino. Está a color (RGBA) en 8 bits sin signo por
+;             canal.
+;   - src:    La imagen origen A. Está a color (RGBA) en 8 bits sin signo por
+;             canal.
+;   - width:  El ancho en píxeles de `src` y `dst`.
+;   - height: El alto en píxeles de `src` y `dst`.
 global ej1
-ej1: ; rdi = dst, rsi = src, rdx = width, rcx = height
-	; prologo
+ej1:
+
+	;prologo
 	push rbp
 	mov rbp, rsp
 
-	; contador de iteraciones necesarias [rcx] = totalPixeles / pixelPorIteracion = width * height / 4
+	;guardamos memoria para los registros volatiles
+	sub rsp, 16*4 
+	movaps [rbp-16], xmm8
+	movaps [rbp-32], xmm9
+	movaps [rbp-48], xmm10
+	movaps [rbp-64], xmm11
+	;movaps [rbp-80], xmm12
+
+	;guardamos en registros los coeficientes y las máscaras para operar mejor
+	movdqu xmm0, [coef_red]
+	movdqu xmm1, [coef_green]
+	movdqu xmm2, [coef_blue]
+	movdqu xmm3, [coef_alph]
+	movdqu xmm8, [mask_coef_red]
+	movdqu xmm9, [mask_coef_green]
+	movdqu xmm10, [mask_coef_blue]
+	movdqu xmm11, [mask_shuf]
+	; movdqu xmm12, [mask_xor]
+
+	; contador de iteraciones necesarias [r9] = totalPixeles / pixelPorIteracion = width * height / 4
 	; 16 bytes por iteracion y 4 bytes por pixel --> 4 pixeles por iteracion 
-	mov rax, rdx
-	imul rax, rcx	; rax = width * height
-	shr rax, 2		; rax = rax / 4
-	mov rcx, rax	; rcx = rax = #iteraciones
+	xor r8, r8 
+	mov r9, rdx 
+	imul r9, rcx
+	shr r9, 2 
 
-	; cargo los coeficientes
-    movdqu xmm1, [coef_red]
-    movdqu xmm2, [coef_green]
-    movdqu xmm3, [coef_blue]
+	.loop:
+		cmp r8, r9
+		je .fin
 
-	; loop principal
-	.ciclo: 
-		; cargo los primeros 4 píxeles en xmm0
-		movdqu xmm0, [rsi]	    ; xmm0 = | (a1,r1,g1,b1) | (a2,r2,g2,b2) | ... | ... |
+		movdqu xmm4, [rsi] ; leo 4 pixeles de src (1 pixel = 4 bytes)
+		; xmm4 = a0 b0 g0 r0 | a1 b1 g1 r1 | a2 b2 g2 r2 | a3 b3 g3 r3
+	
+		; parte roja
+		movdqa xmm5, xmm4	; xmm5 = xmm4
+		pand xmm5, xmm8	    ; xmm5 = 00_00_00_rr | ...
+		cvtdq2ps xmm5, xmm5 ; packed doubleword 2 packed single.
+							; convierto de integer a float (es doubleword porque quedo 00_00_00_RR, 32 bits)
+		mulps xmm5, xmm0    ; xmm5 = |(0,0,0,0.2126*r0)|(0,0,0,0.2126*r1)|
 
-		; componente roja
-		movdqa xmm4, xmm0 
-		pand xmm4, [mask_red]   ; xmm4 = | (0,r1,0,0) | (0,r2,0,0) | ... | ... | 
-		psrld xmm4, 16  	    ; xmm4 = | (0,0,0,r1) | (0,0,0,r2) | ... | ... |
+		; parte verde
+		movdqa xmm6, xmm4	; xmm6 = xmm4
+		pand xmm6, xmm9		; xmm6 = 00_00_gg_00 | ...
+		psrld xmm6, 8 		; xmm6 = 00_00_00_gg | ...
+		cvtdq2ps xmm6, xmm6 ; integer -> float
+		mulps xmm6, xmm1 	; xmm6 = (0,0,0,0.7152*g0) | ... 
 
-		; componente verde
-		movdqa xmm5, xmm0
-		pand xmm5, [mask_green] ; xmm5 = | (0,0,g1,0) | (0,0,g2,0) | ... | ... |
-		psrld xmm5, 8           ; xmm5 = | (0,0,0,g1) | (0,0,0,g2) | ... | ... |
+		; parte azul (se podria no usar xmm7 y pisar directo el xmm4...)
+		movdqa xmm7, xmm4	; xmm7 = xmm4
+		pand xmm7, xmm10	; xmm7 = 00_bb_00_00 | ...
+		psrld xmm7, 16 		; xmm7 = 00_00_00_bb | ...
+		cvtdq2ps xmm7, xmm7	; integer -> float
+		mulps xmm7, xmm2	; xmm7 = (0,0,0,0.0722*b) | ...
 
-		; componente azul
-		movdqa xmm6, xmm0
-		pand xmm6, [mask_blue]  ; xmm6 = | (0,0,0,b1) | (0,0,0,b2) | ... | ... |
+		; xmm5 = (0,0,0,0.0722*b0+0.7152*g0+0.2126*r0) | ...
+		; xmm5 = (0,0,0,lum0) | ...
+		addps xmm5, xmm6
+		addps xmm5, xmm7
 
-		; convertimos a flotante para multiplicar por los coeficientes float
-		cvtdq2ps xmm4, xmm4
-		cvtdq2ps xmm5, xmm5
-		cvtdq2ps xmm6, xmm6
+		; xmm5 = lum0 | lum1 | ...
+		cvtps2dq xmm5, xmm5 ; cada lum ocupa 32 bits (4 bytes) (integer)
+		packusdw xmm5, xmm5 ; cada lum ocupa 16 bits (2 byte)
+		packuswb xmm5, xmm5 ; cada lum ocupa 8 bits (1 byte)
 
-		; multiplicamos por los coeficientes
-		mulps xmm4, xmm1 	   ; xmm4 = | (0,0,0,0.2126*r1) | (0,0,0,0.2126*r2) | ... | ... |
-		mulps xmm5, xmm2	   ; xmm5 = | (0,0,0,0.7152*g1) | (0,0,0,0.7152*g2) | ... | ... |
-		mulps xmm6, xmm3	   ; xmm6 = | (0,0,0,0.0722*b1) | (0,0,0,0.0722*b2) | ... | ... |
+		; shuffle para que cada "pixel" tenga su luminosidad replicada en sus 4 bytes
+		pshufb xmm5, xmm11  ; xmm5 = l3 l3 l3 l3 | l2 l2 l2 l2 | ... 
+							; la mascara está en xmm11 y tiene las posiciones de memoria
+		
+		; pisamos el primer byte con la máscara de ALPHA (0xFF_00_00_00)
+		por xmm5, xmm3      ; xmm5 = FF l3 l3 l3 | FF l2 l2 l2 | ... 
 
-		; sumamos los resultados
-		addps xmm4, xmm5 	   ; xmm4 = | (0,0,0,0.2126*r1+0.7152*g1)           | (0,0,0,0.2126*r2+0.7152*g2) 	        | ... | ... |
-		addps xmm4, xmm6	   ; xmm4 = | (0,0,0,0.2126*r1+0.7152*g1+0.0722*b1) | (0,0,0,0.2126*r2+0.7152*g2+0.0722*b2) | ... | ... |
-
-		; convertimos a entero 
-		cvttps2dq xmm4, xmm4   ; xmm4 = | (0,0,0,lum1) | (0,0,0,lum2) | ... | ... |
-
-        ; copio los valores de luminosidad en los 4 canales de los 4 pixeles
-        pshufd xmm4, xmm4, 0   ; xmm4 = | (lum1,lum1,lum1,lum1) | (lum2,lum2,lum2,lum2) | ... | ... |
-
-		; agrego el canal alpha
-		por xmm4, [mask_alpha] ; xmm4 = | (255,lum1,lum1,lum1) | (255,lum2,lum2,lum2) | ... | ... |
-
-		; guardo los 4 pixeles en dst
-		movdqu [rdi], xmm4
-
-		; avanzo a los siguientes 4 pixeles de src y dst
-		add rdi, 16
+		; escribo los 4 pixeles en dst
+		movdqu [rdi], xmm5 
+		
+		; avanzo 4 pixeles en los punteros a memoria (16 bytes)
 		add rsi, 16
-		loop .ciclo
+		add rdi, 16
 
-	; epilogo
-	pop rbp
-	ret
+		; avanzo el contador de iteraciones
+		add r8, 1
+		jmp .loop
+ 
+	.fin:
+		movaps xmm8, [rbp-16]  ; Restaura xmm8
+		movaps xmm9, [rbp-32]  ; Restaura xmm9
+		movaps xmm10, [rbp-48] ; Restaura xmm10
+		movaps xmm11, [rbp-64] ; Restaura xmm11
+		;movaps xmm12, [rbp-80] ; Restaura xmm12
+		add rsp, 16*4
+		pop rbp
+		ret
