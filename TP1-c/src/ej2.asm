f4721e6e151807b840b712a82c63e48269f8d996
Pedro <pedrofuentes7799@gmail.com>
2024-09-15 19:21:30 -0300

    primera implementacion del ej2

--- TP1-c/src/ej2.asm
+++ TP1-c/src/ej2.asm
@@ -1,6 +1,12 @@
 section .rodata
 ; Poner acá todas las máscaras y coeficientes que necesiten para el filtro
+	mask_alph: times 4 dd 0xFF_00_00_00
+	mask_coef_blue:  times 4 dd 0x00_FF_00_00
+	mask_coef_green: times 4 dd 0x00_00_FF_00
+	mask_coef_red:   times 4 dd 0x00_00_00_FF
 
+	mask_pedro1: db 0x80, 0x80, 0x80, 0x0E, 0x80, 0x0D, 0x80, 0x0C, 0x80, 0x80, 0x80, 0x0A, 0x80, 0x09, 0x80, 0x08
+	mask_pedro2: db 0x80, 0x80, 0x80, 0x06, 0x80, 0x05, 0x80, 0x04, 0x80, 0x80, 0x80, 0x02, 0x80, 0x01, 0x80, 0x00
 section .text
 
 ; Marca un ejercicio como aún no completado (esto hace que no corran sus tests)
@@ -68,5 +74,63 @@ ej2:
 	; r/m32 = uint32_t width
 	; r/m32 = uint32_t height
 
-	ret
+	;prologo
+	push rbp
+	mov rbp, rsp
+
+	; contador de iteraciones necesarias [r9] = totalPixeles / pixelPorIteracion = width * height / 4
+	; 16 bytes por iteracion y 4 bytes por pixel --> 4 pixeles por iteracion 
+	xor r8, r8 
+	mov r9, rdx 
+	imul r9, rcx
+	shr r9, 2 
+
+	; guardamos las máscaras usadas en registros xmm
+	movdqu xmm12, [mask_alph]
+	movdqu xmm8, [mask_coef_red]
+	movdqu xmm9, [mask_coef_green]
+	movdqu xmm10, [mask_coef_blue]
+
+	.loop:
+		cmp r8, r9
+		je .fin
+
+		movdqu xmm4, [rsi] ; leo 4 pixeles de src (1 pixel = 4 bytes)
+		; xmm4 = a0 b0 g0 r0 | a1 b1 g1 r1 | a2 b2 g2 r2 | a3 b3 g3 r3
+
+		; copiamos el dato para operarlo
+		movdqu xmm5, xmm4
+
+		; saco el primer byte de cada pixel 
+		pandn xmm5, xmm12 	
+
+
+
+		; asumo que tengo en xmm5 = 00_0b0_0g0_0r0 | 00_0b1_0g1_0r1
+		; asumo que tengo en xmm6 = 00_0b2_0g2_0r2 | 00_0b3_0g3_0r3
+
+		phaddw xmm5, xmm5 ; xmm5 = 00
+		phaddw xmm6, xmm6 ; 
+		;
+		phaddd xmm6, xmm5
+		; xmm6 = t0 t1 t2 t3
+		
+
+
+
+		; escribo los 4 pixeles en dst
+		movdqu [rdi], xmm5 
+		
+		; avanzo 4 pixeles en los punteros a memoria (16 bytes)
+		add rsi, 16
+		add rdi, 16
+
+		; avanzo el contador de iteraciones
+		add r8, 1
+		jmp .loop
+ 
+	.fin:
+		pop rbp
+		ret
+
 
