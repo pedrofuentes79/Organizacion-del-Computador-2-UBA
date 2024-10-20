e3cd1f37d64938fe1bb126fc7f9ff6f90889d40e
Pedro <pedrofuentes7799@gmail.com>
2024-10-20 19:19:23 -0300

    avanzo con otras funciones

--- TPSP/src/mmu.c
+++ TPSP/src/mmu.c
@@ -75,15 +75,14 @@ paddr_t mmu_next_free_user_page(void) {
  * @return devuelve la dirección de memoria de la página donde se encuentra el directorio
  * de páginas usado por el kernel
  */
+
 paddr_t mmu_init_kernel_dir(void) {
   // para hacer el identity mapping, la idea es tener un directorio de tablas de paginas de kernel
   // de 0x0 a 0x3FFFFF necesito 1024 paginas. Osea, una tabla de paginas :)
 
-  // inicializamos el directorio de paginas
-
   // limpio el dir y la tabla
-  kmemset(kpd, 0x00, PAGE_SIZE);
-  kmemset(kpt, 0x00, PAGE_SIZE);
+  zero_page((paddr_t)kpd);
+  zero_page((paddr_t)kpt);
 
   // la primera entrada de la kpd va a tener la direccion de memoria de la kpt 
   // ponemos kpt[0] porque la direccion de memoria de la tabla de paginas es la su primer entrada
@@ -92,56 +91,105 @@ paddr_t mmu_init_kernel_dir(void) {
   kpd[0].pt = MMU_ENTRY_PADDR((uint32_t)&kpt[0]) >> 12;
   kpd[0].attrs = MMU_P | MMU_W; // present y (writable?)
 
+
   // identity mapping
-  uint32_t number_of_pages = identity_mapping_end / PAGE_SIZE;
-  for (uint32_t i = 0; i <= number_of_pages; i++){
-    // mmu_map_page((uint32_t)kpd, i * PAGE_SIZE, i * PAGE_SIZE, MMU_P | MMU_W);
-    kpt[i].page = i;
+  paddr_t free_page = mmu_next_free_kernel_page();
+  uint32_t i = 0;
+  while (free_page <= identity_mapping_end) {
+    zero_page(free_page);
+    kpt[i].page = free_page >> 12;
     kpt[i].attrs = MMU_P | MMU_W;
+
+    free_page = mmu_next_free_kernel_page();
+    i++;
   }
 
   return (paddr_t) kpd;
 
 }
 
+/**
+ * mmu_map_page agrega las entradas necesarias a las estructuras de paginación de modo de que
+ * la dirección virtual virt se traduzca en la dirección física phy con los atributos definidos en attrs
+ * @param cr3 el contenido que se ha de cargar en un registro CR3 al realizar la traducción
+ * @param virt la dirección virtual que se ha de traducir en phy
+ * @param phy la dirección física que debe ser accedida (dirección de destino)
+ * @param attrs los atributos a asignar en la entrada de la tabla de páginas
+ */
+void mmu_map_page(uint32_t cr3, vaddr_t virt, paddr_t phy, uint32_t attrs) {
+
+  pd_entry_t* pd = (pd_entry_t*)CR3_TO_PAGE_DIR(cr3);
+  uint32_t pd_index = VIRT_PAGE_DIR(virt);
+  pt_entry_t* pt = (pt_entry_t*)MMU_ENTRY_PADDR(pd[pd_index].pt);  
+  uint32_t pt_index = VIRT_PAGE_TABLE(virt);
+
+  pt[pt_index].page = phy >> 12;
+  pt[pt_index].attrs = attrs;
+
+  tlbflush();
+
+}
+
+/**
+ * mmu_unmap_page elimina la entrada vinculada a la dirección virt en la tabla de páginas correspondiente
+ * @param virt la dirección virtual que se ha de desvincular
+ * @return la dirección física de la página desvinculada
+ */
+paddr_t mmu_unmap_page(uint32_t cr3, vaddr_t virt) {
+  pd_entry_t* pd = (pd_entry_t*)CR3_TO_PAGE_DIR(cr3);
+  uint32_t pd_index = VIRT_PAGE_DIR(virt);
+  pt_entry_t* pt = (pt_entry_t*)MMU_ENTRY_PADDR(pd[pd_index].pt);
+  uint32_t pt_index = VIRT_PAGE_TABLE(virt);
+
+  // chequeo si esta presente
+  if (!(pt[pt_index].attrs & MMU_P)) {
+    return 0;
+  }
+
+  paddr_t page_to_unmap = (pt[pt_index].page << 12) | VIRT_PAGE_OFFSET(virt);
+
+  // seteo la pagina como "no presente"
+  pt[pt_index].attrs = pt[pt_index].attrs & (~MMU_P);
+  // podria ponerla toda en ceros?
+
+  tlbflush();
+  return page_to_unmap;
+}
+
+#define DST_VIRT_PAGE 0xA00000
+#define SRC_VIRT_PAGE 0xB00000
+
+/**
+ * copy_page copia el contenido de la página física localizada en la dirección src_addr a la página física ubicada en dst_addr
+ * @param dst_addr la dirección a cuya página queremos copiar el contenido
+ * @param src_addr la dirección de la página cuyo contenido queremos copiar
+ *
+ * Esta función mapea ambas páginas a las direcciones SRC_VIRT_PAGE y DST_VIRT_PAGE, respectivamente, realiza
+ * la copia y luego desmapea las páginas. Usar la función rcr3 definida en i386.h para obtener el cr3 actual
+ */
+void copy_page(paddr_t dst_addr, paddr_t src_addr) {
+  // necesito mapearlas a alguna direccion virtual! 
+  // si no, no puedo acceder a ellas para hacer la copia
+  mmu_map_page(rcr3(), DST_VIRT_PAGE, dst_addr, MMU_P | MMU_W);
+  mmu_map_page(rcr3(), SRC_VIRT_PAGE, src_addr, MMU_P | MMU_W);
+
+  // obtengo los punteros a cada una
+  uint8_t* src = (uint8_t*)SRC_VIRT_PAGE;
+  uint8_t* dst = (uint8_t*)DST_VIRT_PAGE;
+
+  // copio
+  for (size_t i = 0; i < PAGE_SIZE; i++) {
+    dst[i] = src[i];
+  }
+
+  // las desmapeo de estas direcciones virtuales auxiliares
+  mmu_unmap_page(rcr3(), DST_VIRT_PAGE);
+  mmu_unmap_page(rcr3(), SRC_VIRT_PAGE);
+}
+
 // /**
-//  * mmu_map_page agrega las entradas necesarias a las estructuras de paginación de modo de que
-//  * la dirección virtual virt se traduzca en la dirección física phy con los atributos definidos en attrs
-//  * @param cr3 el contenido que se ha de cargar en un registro CR3 al realizar la traducción
-//  * @param virt la dirección virtual que se ha de traducir en phy
-//  * @param phy la dirección física que debe ser accedida (dirección de destino)
-//  * @param attrs los atributos a asignar en la entrada de la tabla de páginas
-//  */
-// void mmu_map_page(uint32_t cr3, vaddr_t virt, paddr_t phy, uint32_t attrs) {
-
-// }
-
-// /**
-//  * mmu_unmap_page elimina la entrada vinculada a la dirección virt en la tabla de páginas correspondiente
-//  * @param virt la dirección virtual que se ha de desvincular
-//  * @return la dirección física de la página desvinculada
-//  */
-// paddr_t mmu_unmap_page(uint32_t cr3, vaddr_t virt) {
-
-// }
-
-// #define DST_VIRT_PAGE 0xA00000
-// #define SRC_VIRT_PAGE 0xB00000
-
-// /**
-//  * copy_page copia el contenido de la página física localizada en la dirección src_addr a la página física ubicada en dst_addr
-//  * @param dst_addr la dirección a cuya página queremos copiar el contenido
-//  * @param src_addr la dirección de la página cuyo contenido queremos copiar
-//  *
-//  * Esta función mapea ambas páginas a las direcciones SRC_VIRT_PAGE y DST_VIRT_PAGE, respectivamente, realiza
-//  * la copia y luego desmapea las páginas. Usar la función rcr3 definida en i386.h para obtener el cr3 actual
-//  */
-// void copy_page(paddr_t dst_addr, paddr_t src_addr) {
-// }
-
-//  /**
 //  * mmu_init_task_dir inicializa las estructuras de paginación vinculadas a una tarea cuyo código se encuentra en la dirección phy_start
-//  * @pararm phy_start es la dirección donde comienzan las dos páginas de código de la tarea asociada a esta llamada
+//  * @param phy_start es la dirección donde comienzan las dos páginas de código de la tarea asociada a esta llamada
 //  * @return el contenido que se ha de cargar en un registro CR3 para la tarea asociada a esta llamada
 //  */
 // paddr_t mmu_init_task_dir(paddr_t phy_start) {
