775aa58e16632c965ffa2a53a68024c243d7a92f
vtenconi <tenconivini@gmail.com>
2024-10-30 17:42:01 -0300

    quedo feito pero ahi esta la implementacion

--- TPSP/src/tareas/taskPongScoreboard.c
+++ TPSP/src/tareas/taskPongScoreboard.c
@@ -5,8 +5,6 @@
 
 #define SHARED_SCORE_BASE_VADDR (PAGE_ON_DEMAND_BASE_VADDR + 0xF00)
 #define CANT_PONGS 3
-
-
 void task(void) {
 	screen pantalla;
 	// ¿Una tarea debe terminar en nuestro sistema?
@@ -15,6 +13,18 @@ void task(void) {
 	// Completar:
 	// - Pueden definir funciones auxiliares para imprimir en pantalla
 	// - Pueden usar `task_print`, `task_print_dec`, etc. 
-		syscall_draw(pantalla);
+	uint8_t h = 5;
+	// armo un while para los tres pongs
+		for (int i = 0; i < CANT_PONGS; i++)
+		{
+			uint32_t* current_record = (uint32_t*) SHARED_SCORE_BASE_VADDR + (i * sizeof(uint32_t)*2);
+			// Imprimo el puntaje de los jugadores
+            task_print(pantalla, "Puntaje jugador 1: ", 0, h, C_FG_WHITE);
+			task_print_dec(pantalla, current_record[0], 2, WIDTH * 0.75 + 3, h, C_FG_CYAN);
+			task_print(pantalla, "Puntaje jugador 2: ", 0, h+1, C_FG_WHITE);
+			task_print_dec(pantalla, current_record[1], 2, WIDTH * 0.75 + 3, h+1, C_FG_CYAN);
+			syscall_draw(pantalla);
+			h += 4;
+		}
 	}
 }
